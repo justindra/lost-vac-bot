2afa6f0dcdd5531275093c31fd79ac5292508df2
Justin Rahardjo <justindra91@gmail.com>
2026-01-23 20:07:23 -0800

    feat: initial commit

---
+++ app.config.ts
@@ -0,0 +1,49 @@
+import type { ExpoConfig, ConfigContext } from "expo/config";
+import { version } from "./package.json";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "lost-roomba",
+  slug: "lost-roomba",
+  version,
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "lostroomba",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      backgroundColor: "#E6F4FE",
+      foregroundImage: "./assets/images/android-icon-foreground.png",
+      backgroundImage: "./assets/images/android-icon-background.png",
+      monochromeImage: "./assets/images/android-icon-monochrome.png",
+    },
+    edgeToEdgeEnabled: true,
+  },
+  web: {
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+        dark: {
+          backgroundColor: "#000000",
+        },
+      },
+    ],
+  ],
+  experiments: {
+    typedRoutes: true,
+    reactCompiler: true,
+  },
+});
